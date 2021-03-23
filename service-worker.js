e6f7480b64acb4e228ce3929d6019a6feb6efcc9
giorgiopeng <84867579@qq.com>
2021-03-23 18:33:41 +0800

    9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FYP/precache-manifest.5b19538365a0d150121a953ebd93b838.js"
+  "/FYP/precache-manifest.f59cb945be22efca6b61115b621fe005.js"
 );
 
 self.addEventListener('message', (event) => {
